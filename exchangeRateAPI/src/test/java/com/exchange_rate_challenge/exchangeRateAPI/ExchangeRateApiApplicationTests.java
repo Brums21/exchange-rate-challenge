803aa74b5d3e87bd97dd7a66c3320ab8dc22247a
Bruna Melo <brunams21@ua.pt>
2025-12-16 01:08:14 +0000

    Spring Boot project initialization

---
+++ exchangeRateAPI/src/test/java/com/exchange_rate_challenge/exchangeRateAPI/ExchangeRateApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exchange_rate_challenge.exchangeRateAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExchangeRateApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
